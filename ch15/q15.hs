1297bfd7b79d7d15bb106588bb4e5efc98728f7b
Mike Harris <MikeMKH@users.noreply.github.com>
2020-10-13 06:16:14 -0500

    add examples 15.1

---
+++ ch15/q15.hs
@@ -0,0 +1,35 @@
+data FourLetterAlphabet = L1 | L2 | L3 | L4 deriving (Show, Enum, Bounded)
+data ThreeLetterAlphabet = Alpha | Beta | Kappa deriving (Show, Enum, Bounded)
+
+rotN :: (Bounded a, Enum a) => Int -> a -> a
+rotN alphabetSize c = toEnum rotation
+  where
+    halfAlphabetSize = alphabetSize `div` 2
+    offset = fromEnum c + halfAlphabetSize
+    rotation = offset `mod` alphabetSize
+    
+rotNDecoder :: (Bounded a, Enum a) => Int -> a -> a
+rotNDecoder n c = toEnum rotation
+  where
+    halfN = n `div` 2
+    offset = if even n
+      then fromEnum c + halfN
+      else 1 + fromEnum c + halfN
+    rotation = offset `mod` n
+    
+rotChar :: Char -> Char
+rotChar c = rotN alphabetSize c
+  where
+    alphabetSize = 1 + fromEnum (maxBound :: Char)
+
+rotEncoder :: String -> String
+rotEncoder text = map rotChar text
+  where
+    alphabetSize = 1 + fromEnum (maxBound :: Char)
+    rotChar = rotN alphabetSize
+
+rotDecoder :: String -> String
+rotDecoder text = map rotChar text
+  where
+    alphabetSize = 1 + fromEnum (maxBound :: Char)
+    rotChar = rotNDecoder alphabetSize
