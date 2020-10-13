ef72bab30c128eb4a1f8f0dc73a980f4be2995b5
Mike Harris <MikeMKH@users.noreply.github.com>
2020-10-13 06:21:59 -0500

    add examples 15.2

--- ch15/q15.hs
+++ ch15/q15.hs
@@ -33,3 +33,12 @@ rotDecoder text = map rotChar text
   where
     alphabetSize = 1 + fromEnum (maxBound :: Char)
     rotChar = rotNDecoder alphabetSize
+
+xorBool :: Bool -> Bool -> Bool
+xorBool x y = (x || y) && (not (x && y))
+
+xorPair :: (Bool, Bool) -> Bool
+xorPair (x, y) = xorBool x y
+
+xor :: [Bool] -> [Bool] -> [Bool]
+xor xs ys = map xorPair $ zip xs ys
