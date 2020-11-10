50fbdc6225e3d0f127d35ab043dfface401539de
Mike Harris <MikeMKH@users.noreply.github.com>
2020-11-10 06:35:19 -0600

    add examples 26.2.1 - 26.2.4

--- ch26/marc_to_html.hs
+++ ch26/marc_to_html.hs
@@ -46,6 +46,55 @@ booksToHtml books = mconcat [
 books :: [Book]
 books = [book1, book2, book3]
 
+type MarcRecordRaw = B.ByteString
+type MarcLeaderRaw = B.ByteString
+
+leaderLength :: Int
+leaderLength = 24
+
+getLeader :: MarcRecordRaw -> MarcLeaderRaw
+getLeader record = B.take leaderLength record
+
+rawToInt :: B.ByteString -> Int
+rawToInt = read . T.unpack . E.decodeUtf8
+
+getRecordLength :: MarcLeaderRaw -> Int
+getRecordLength leader = rawToInt $ B.take 5 leader
+
+nextAndRest :: B.ByteString -> (MarcRecordRaw, B.ByteString)
+nextAndRest stream = B.splitAt recordLength stream
+  where
+    recordLength = getRecordLength stream
+
+allRecords :: B.ByteString -> [MarcRecordRaw]
+allRecords stream =
+  if stream == B.empty
+    then []
+    else next : allRecords rest
+  where
+    (next, rest) = nextAndRest stream
+
+type MarcDirectoryRaw = B.ByteString
+
+getBaseAddress :: MarcLeaderRaw -> Int
+getBaseAddress leader = rawToInt $ B.take 5 $ B.drop 12 leader
+
+getDirectoryLength :: MarcLeaderRaw -> Int
+getDirectoryLength leader = getBaseAddress leader - (leaderLength + 1)
+
+getDirectory :: MarcRecordRaw -> MarcDirectoryRaw
+getDirectory record = B.take directoryLength afterLeader
+  where
+    directoryLength = getDirectoryLength record
+    afterLeader = B.drop leaderLength record
+
+-- downloaded from https://archive.org/download/marc_oregon_summit_records/catalog_files/
+-- renamed to sample.mrc and placed in same directory as source file
+marcFileName :: String
+marcFileName = "sample.mrc"
+
 main :: IO()
 main = do
-  TIO.writeFile "books.html" $ booksToHtml books
+  raw <- B.readFile marcFileName
+  let records = allRecords raw
+  print $ length records
