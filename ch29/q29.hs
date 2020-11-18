46d9d41109dddddabf33be405acc9086aace4b2f
Mike Harris <MikeMKH@users.noreply.github.com>
2020-11-18 06:46:33 -0600

    add q29.1 - q29.3

--- ch29/q29.hs
+++ ch29/q29.hs
@@ -3,3 +3,33 @@ q29_1 = (++) <$> Just "Mike " <*> Just "Harris"
 
 q29_2 :: IO String
 q29_2 = pure "Hello World"
+
+allFmap :: Applicative f => (a -> b) -> f a -> f b
+allFmap func x = pure func <*> x
+
+example :: Int
+example = (*) ((+) 2 4) 6
+
+exampleMaybe :: Maybe Int
+exampleMaybe = pure (*) <*> (pure (+) <*> pure 2 <*> pure 4) <*> pure 6
+
+initialBeer :: [Integer]
+initialBeer = [6, 12]
+
+numberOfBeer :: [Integer]
+numberOfBeer = (-) 4 <$> initialBeer
+
+numberOfFriends :: [Integer]
+numberOfFriends = [2, 3]
+
+totalPeople :: [Integer]
+totalPeople = (+) 2 <$> numberOfFriends
+
+averageNumberOfBeerDrank :: [Integer]
+averageNumberOfBeerDrank = [3, 4]
+
+numberOfBeerNeeded :: [Integer]
+numberOfBeerNeeded = (*) <$> totalPeople <*> averageNumberOfBeerDrank
+
+beerToBuy :: [Integer]
+beerToBuy = (-) <$> numberOfBeerNeeded <*> numberOfBeer
