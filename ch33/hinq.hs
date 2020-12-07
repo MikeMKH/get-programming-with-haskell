65686140315de90f3da2b7a956e5f6e653f72f39
Mike Harris <MikeMKH@users.noreply.github.com>
2020-12-07 06:39:36 -0600

    add examples 33.4 - 33.6

--- ch33/hinq.hs
+++ ch33/hinq.hs
@@ -1,11 +1,12 @@
 import Control.Monad
+import Control.Applicative
 
 data Name = Name
   { firstName :: String
   , lastName :: String }
 
 instance Show Name where
-  show (Name first last) = mconcat [ last, ", ", first ]
+  show (Name first last) = mconcat [ first, " ", last ]
 
 data GradeLevel = Freshman
                 | Sophomore
@@ -42,12 +43,12 @@ courses :: [Course]
 courses = [(Course 100 "Life of Cats" 1)
           ,(Course 200 "Dog Breeds" 2)]
 
-_select :: (a -> b) -> [a] -> [b]
+_select :: Monad m => (a -> b) -> m a -> m b
 _select accessor xs = do
   x <- xs
   return $ accessor x
 
-_where :: (a -> Bool) -> [a] -> [a]
+_where :: (Monad m, Alternative m) => (a -> Bool) -> m a -> m a
 _where test xs = do
   x <- xs
   guard $ test x
@@ -56,10 +57,54 @@ _where test xs = do
 startsWith :: Char -> String -> Bool
 startsWith letter s = letter == head s
 
-_join :: Eq c => [a] -> [b] -> (a -> c) -> (b -> c) -> [(a, b)]
+_join :: (Monad m, Alternative m, Eq c) => m a -> m b -> (a -> c) -> (b -> c) -> m (a, b)
 _join xs ys accessorX accessorY = do
   x <- xs
   y <- ys
   let pairs = (x, y)
   guard $ accessorX (fst pairs) == accessorY (snd pairs)
   return pairs
+
+_hinq :: (t1 -> t2) -> t3 -> (t3 -> t1) -> t2
+_hinq selectQuery joinQuery whereQuery =
+  (\joinData ->
+    (\whereResults -> selectQuery whereResults) (whereQuery joinData)) joinQuery
+
+data HINQ m a b = HINQ (m a -> m b) (m a) (m a -> m a)
+                | HINQ_ (m a -> m b) (m a)
+
+runHINQ :: (Monad m, Alternative m) => HINQ m a b -> m b
+runHINQ (HINQ s j w) = _hinq s j w
+runHINQ (HINQ_ s j) = _hinq s j (_where (\_ -> True))
+
+
+queryTeacherName :: HINQ [] Teacher Name
+queryTeacherName = HINQ_
+  (_select teacherName)
+  teachers
+
+queryTeacherNameWhoTeachLifeOfCats :: HINQ [] (Teacher, Course) Name
+queryTeacherNameWhoTeachLifeOfCats = HINQ
+  (_select (teacherName . fst))
+  (_join teachers courses teacherId courseTeacher)
+  (_where ((== "Life of Cats") . courseTitle . snd))
+
+possibleTeacher :: Maybe Teacher
+possibleTeacher = Just $ head teachers
+
+possibleCourse :: Maybe Course
+possibleCourse = Just $ head courses
+
+queryMaybeJust :: HINQ Maybe (Teacher, Course) Name
+queryMaybeJust = HINQ_
+  (_select (teacherName . fst))
+  (_join possibleTeacher possibleCourse teacherId courseTeacher)
+
+missingCourse :: Maybe Course
+missingCourse = Nothing
+
+queryMaybeNothing :: HINQ Maybe (Teacher, Course) Name
+queryMaybeNothing = HINQ
+  (_select (teacherName . fst))
+  (_join possibleTeacher missingCourse teacherId courseTeacher)
+  (_where ((== "The Joy of Flash") . courseTitle . snd))
