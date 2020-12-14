60d11593d17f2f19ed76923dddef4823c6d031c4
Mike Harris <MikeMKH@users.noreply.github.com>
2020-12-14 06:39:47 -0600

    add examples 37.4 - 37.5

--- ch37/primes/src/Primes.hs
+++ ch37/primes/src/Primes.hs
@@ -14,9 +14,24 @@ sieve :: [Int] -> [Int]
 sieve [] = []
 sieve (p : xs) = p : sieve candidates
   where
-      candidates = filter (not . isDivisibleBy p) xs
+    candidates = filter (not . isDivisibleBy p) xs
 
 isPrime :: Int -> Maybe Bool
-isPrime n | n < 0 = Nothing
+isPrime n | n < 2 = Nothing
           | n > maxPrime = Nothing
           | otherwise = Just $ n `elem` primes
+
+unsafePrimeFactors :: Int -> [Int] -> [Int]
+unsafePrimeFactors _ [] = []
+unsafePrimeFactors n (candidate:xs) =
+    if n `mod` candidate == 0
+      then candidate : unsafePrimeFactors (n `div` candidate) (candidate:xs)
+      else unsafePrimeFactors n xs
+
+primeFactors :: Int -> Maybe [Int]
+primeFactors n | n < 2 = Nothing
+               | n >= maxPrime = Nothing
+               | otherwise = Just $ unsafePrimeFactors n primesLessThanN
+  where
+    primesLessThanN = filter (<= n) primes
+                     
--- ch37/primes/test/Spec.hs
+++ ch37/primes/test/Spec.hs
@@ -1,2 +1,49 @@
+import Test.QuickCheck
+import Primes
+import Data.Maybe
+
+prop_validPrimesOnly value =
+  if value < 2 || value >= maxPrime
+    then isNothing result
+    else isJust result
+  where
+    result = isPrime value
+
+prop_primesArePrime value =
+  if result == Just True
+    then length divisors == 0
+    else True
+  where
+    result = isPrime value
+    divisors = filter ((== 0) . (value `mod`)) [2 .. (value - 1)]
+
+prop_nonPrimesAreComposite value =
+  if result == Just False
+    then length divisors > 0
+    else True
+  where
+    result = isPrime value
+    divisors = filter ((== 0) . (value `mod`)) [2 .. (value - 1)]
+
+prop_factorsMakeOriginal value =
+  if isNothing result
+    then True
+    else product (fromJust result) == value
+  where
+    result = primeFactors value
+
+prop_allFactorsArePrime value =
+  if isNothing result
+    then True
+    else all (== Just True) resultArePrimes
+  where
+    result = primeFactors value
+    resultArePrimes = map isPrime (fromJust result)
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+  quickCheck prop_validPrimesOnly
+  quickCheckWith stdArgs { maxSuccess = 1000 } prop_primesArePrime
+  quickCheckWith stdArgs { maxSuccess = 1000 } prop_nonPrimesAreComposite
+  quickCheckWith stdArgs { maxSuccess = 1000 } prop_factorsMakeOriginal
+  quickCheckWith stdArgs { maxSuccess = 1000 } prop_allFactorsArePrime
